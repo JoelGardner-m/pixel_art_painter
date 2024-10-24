0002a7bed4e9c37ce4c8019ea54a0e2c9ba9e64b
JoelGardner-m <joel.gardnerm@gmail.com>
2024-10-24 15:12:48 -0400

    app ui structured

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div id="top"> 
+        <div id="menu">
+          <button>file</button>
+          <button>edit</button>
+          <button>preference</button>
+
+        </div>
+        <div id="animations">
+          {/*animtions */}
+
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id="middle">
+        <div id="tool-bar">
+
+
+        </div>
+        <div id="canvas-space">
+          <canvas></canvas>
+          <script></script>
+        </div>
+        <div id="color-palete"> 
+        <div id="colors"></div>
+        <div id="color picker"></div>
+        </div>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div id="bottom">
+        <div id="layers"></div>
+        <div id="ads "></div>
+      </div>
+
+    </div>
+    
+    
     </>
   )
 }
