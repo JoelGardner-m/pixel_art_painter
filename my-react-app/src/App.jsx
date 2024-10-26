2fb0b5813eff3bea047c7f1efe5f765fc7470bc4
JoelGardner-m <162535245+JoelGardner-m@users.noreply.github.com>
2024-10-26 16:34:07 +0000

    penceil tool added

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react"
 
+// cd my-react-app && npm install && npm run dev
 function Canvas(props){
+  const id = props.ID
+
   // current array and update array(picture)
   const ratio = 250
   const [canvasWidth, setCanvasWidth] = useState(ratio)
@@ -13,11 +16,26 @@ function Canvas(props){
   const [canvasLoad, setCanvasLoad] = useState(false)
   const [currentImage, setCurrentImage] = useState(generateGridArray())
   const [currentPixelScale, setCurrentPixelScale] = useState(10)
-  const [colorSelective, setColorSelective] = useState({
-      c0: 'rgba(255, 255, 255, 1)'
+  const [colorSelective, setColorSelective] = useState(
+    {
+      c0: 'rgba(255, 255, 255, 1)',
+      c1: 'rgba(0,0,0,1)'
 
   })
-  function pexel (x, y, w, h, color=0, stroke = 'black'){
+
+  function changeColorSelection(newColorSelection){
+    setColorSelective(newColorSelection)
+
+  }
+
+  const [currentColor, setCurrentColor] = useState(1)
+  function changeCurrentColor(int){
+    setCurrentColor(int)
+  }
+  
+
+
+  function pexel (x, y, w, h, color = 0, stroke = 'black'){
     const canvas = document.getElementById(id)
     const ctx = canvas.getContext("2d")
     ctx.beginPath(); // Start a new path
@@ -46,12 +64,11 @@ function Canvas(props){
    
   }
   function generateimage(){
-    const image = generateGridArray()
+    const image = currentImage
     
     for (let y = 0; y < image.length; y++) {
       for (let x = 0; x < image[y].length; x++) {
-        console.log(image[y])
-        pexel(x*currentPixelScale, y*currentPixelScale, currentPixelScale, currentPixelScale, 0)
+        pexel(x*currentPixelScale, y*currentPixelScale, currentPixelScale, currentPixelScale, image[x][y])
         
       }
       
@@ -59,27 +76,88 @@ function Canvas(props){
 
   }
 
+  function getMousePosition(canvas, event) {
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
 
-  const id = props.ID
+    let currentRect = {
+      x: Math.round((x-1.1)/ 10) ,
+      y: Math.round((y-1.1)/ 10) 
+    }
+    
+
+    console.log("Coordinate x: " + currentRect.x,
+      "Coordinate y: " + currentRect.y);
+
+    return currentRect 
+  }
+  // pencil
+  function pencil(position, color){
+    const {x, y} = position
+    //console.log(x ,y)
+    let newArray = Array(...currentImage)
+    newArray[x][y] = color
+    //console.log(newArray)
+    setCurrentImage(newArray)
+  }
+
+  function mouseDetection(){
+  
+  let canvasElem = document.getElementById(id)
+  let mouseOnCanvas = false
+  let mouseHeld = false
+  
+  canvasElem.addEventListener("mouseenter", function (e) {
+    mouseOnCanvas = true
+  }); 
+  canvasElem.addEventListener("mouseexit", function (e) {
+    mouseOnCanvas = false
+  }); 
+
+  canvasElem.addEventListener("mousedown", function (e) {
+    if (mouseOnCanvas) mouseHeld = true
+    const position = getMousePosition(canvasElem, e);
+    pencil(position, currentColor)
+    console.log('d')
+  }); 
+
+  canvasElem.addEventListener("mouseup", function (e) {
+    if (mouseOnCanvas) mouseHeld = false
+    console.log('u')
+  }); 
+
+  canvasElem.addEventListener("mousemove", function (e) {
+    if (mouseHeld == true){
+      const position = getMousePosition(canvasElem, e);
+      pencil(position, currentColor)
+      
+    }
+  }); 
+
+  
+
+  }
+  
   function changeCanvasStatus(){
     setCanvasLoad(true)
   }
 
   useEffect(()=>{
-    
+  mouseDetection()
 
   }, [canvasLoad])
 
   useEffect(()=>{
-   
     generateimage()
+    
 
-  }, [])
+  }, [currentImage])
   
 
   
   return(<>
-  <canvas id={id} onLoad={()=>changeCanvasStatus()} width={canvasWidth} height={canvasHeight} style={{backgroundColor:'rgb(100, 100, 50)'}}></canvas>
+  <canvas id={id} onLoad={()=>changeCanvasStatus()} width={canvasWidth} height={canvasHeight} style={{backgroundColor:'rgb(100, 100, 50)', cursor: 'crosshair' }}></canvas>
   
   </>)
 
